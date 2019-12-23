f6744fb2925fd0768b8c0470702aa5acf0012b3d
Adenilson Santos <adenilson.santos@gocase.com.br>
2019-12-22 22:27:47 -0300

    first page showing images using firebase and migrate app to androidx

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:any_virtual_store/tabs/home_tab.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  final _pageController = PageController();
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _pageController,
+      physics: NeverScrollableScrollPhysics(),
+      children: <Widget>[
+       HomeTab(),
+      ],
+    );
+  }
+}
